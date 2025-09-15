285a662a1ae92f87abfc6088ddfa1e1fd097d1c4
tsepiso2 <tsepisomonokoane24@gmail.com>
2025-09-15 21:23:31 +0200

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,7 @@
-// frontend/src/api.js
 import axios from 'axios';
 
-// Base URL points to deployed backend or local server
 const api = axios.create({
-  baseURL: "https://wings-backend.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
