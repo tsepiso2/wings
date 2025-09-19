748796463b1f05543204a802a3e29dda7a8dc80f
tsepiso2 <tsepisomonokoane24@gmail.com>
2025-09-19 11:08:55 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -133,5 +133,5 @@ app.get('*', (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:5000`);
+  console.log(`✅ Server running on http://localhost:${PORT`);
 });
