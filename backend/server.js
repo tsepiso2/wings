c4e52ebb10a5ffc537a1ce2bdbf4998a33ccb033
tsepiso2 <tsepisomonokoane24@gmail.com>
2025-09-19 14:28:23 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -139,5 +139,5 @@ app.get('*', (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log('https://wings-main.onrender.com');
 });
